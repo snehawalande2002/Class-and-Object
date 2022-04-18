314eaefad08755cc2f59cc7ca2050ee23f62dbdb
Walande Sneha <103844214+snehawalande2002@users.noreply.github.com>
2022-04-18 10:11:12 +0530

    Add files via upload

---
+++ simple_class_object.py
@@ -0,0 +1,17 @@
+class Me:
+
+    def read(self,name,roll):
+        self.nam=name
+        self.r=roll
+
+        print(name)
+        print(roll)
+
+    def write(self):
+        print("yes i am")
+b=Me()
+b.read("sneha",10)
+b.write()
+
+
+
